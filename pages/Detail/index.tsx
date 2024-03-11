de6c7ccc38b490949d361da06ae51087a7b2aead
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-03-11 22:32:44 +0400

    detail style fixes

--- pages/Detail/index.tsx
+++ pages/Detail/index.tsx
@@ -1,8 +1,16 @@
 /* eslint-disable prettier/prettier */
 import React, {useEffect} from 'react';
-import {BackHandler, StyleSheet, Text, View} from 'react-native';
+import {
+  BackHandler,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {setDetailOpen} from '../../redux/slice';
+import {faCircleDown, faHeart} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 
 export default function Detail() {
   useEffect(() => {
@@ -22,9 +30,22 @@ export default function Detail() {
 
   const item = useSelector(state => state.counter.openItem);
   const dispatch = useDispatch();
+  const image = {uri: item.img};
   return (
     <View style={styles.container}>
-      <Text>{item.name}</Text>
+      <ImageBackground source={image} style={styles.image}>
+        <View style={styles.controlsContainer}>
+          <Text>{item.name}</Text>
+          <View style={styles.controls}>
+            <FontAwesomeIcon icon={faCircleDown} />
+            <Text>Set as</Text>
+            <View>
+              <FontAwesomeIcon icon={faHeart} />
+              {/* <FontAwesomeIcon icon={faHeart} /> */}
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -34,5 +55,29 @@ const styles = StyleSheet.create({
     flex: 1,
     // width: 100,
     backgroundColor: 'red',
+  },
+  image: {
+    flex: 1,
+  },
+  controls: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    gap: 20,
+    // flex: 1,
+    // position: 'absolute',
+    // bottom: 20,
+    zIndex: 100,
+    backgroundColor: 'red',
+    left: 0,
+    width: '100%',
+    height: 50,
+  },
+  controlsContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flex: 1,
   },
 });
